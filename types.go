58a0cad23b4b11600a8d74629191cb5b461e5051
Micah Parks <micahleviparks@gmail.com>
2020-03-28 00:27:00 -0400

    Mongo friendly types.

--- types.go
+++ types.go
@@ -1,13 +1,11 @@
 package main
 
-type User struct {
-	Username string
-	Password string
-}
+type Color string
 
-type Query struct {
-	Url  string
-	User string
+type List struct {
+	Id    string
+	Owner string
+	Subs  []string
 }
 
 type Post struct {
@@ -23,55 +21,25 @@ type Post struct {
 	Text        string
 	Title       string
 	Year        int
-}
-
-type QueryPost struct {
-	Id    string
-	Query string
-	Post  string
-}
-
-type UPost struct {
-	Id   string
-	Post string
-	User string
-	Type string
+	Query       Query
 }
 
 type Preset struct {
-	Id    string
-	Color string // Make another type predefined?
-	Make  string
-	Model string
-	Year  int
+	Id      string
+	Color   Color
+	Default bool
+	Make    string
+	Model   string
+	Owner   string
+	Subs    []string
+	Year    int
 }
 
-type PShare struct {
-	Id    string
-	Owner string
+type Query struct {
+	Default bool
+	Url     string
 }
-
-type PShareSubs struct {
-	Id   string
-	User string
-}
-
-type UserPreset struct {
-	Preset string
-	User   string
-}
-
-type List struct {
-	Id    string
-	Owner string
-}
-
-type ListPosts struct {
-	Id   string
-	Post string
-}
-
-type ListSubs struct {
-	Id   string
-	User string
+type User struct {
+	Username string
+	Password string
 }
