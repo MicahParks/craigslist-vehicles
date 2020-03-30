4f4e58d0303f2c8110f987518b63060dc2838944
Micah Parks <micahleviparks@gmail.com>
2020-03-30 14:31:07 -0400

    TODO.

--- gui/presetCan.go
+++ gui/presetCan.go
@@ -35,44 +35,45 @@ func presetPreview(o *orb, owner, sub []*types.Preset) *fyne.Container {
 		widget.NewLabel("subdomains"),
 	)
 	pCon := fyne.NewContainerWithLayout(layout.NewGridLayout(12))
+	// TODO Build pCon from preset.Query.
 	all := append(owner, sub...)
-	for _, own := range all {
+	for _, preset := range all {
 		suffix := ",\n"
 		discards := ""
-		for _, d := range own.Discard {
+		for _, d := range preset.Discard {
 			discards += d + suffix
 		}
 		discards = strings.TrimSuffix(discards, suffix)
 		require := ""
-		for _, r := range own.Required {
+		for _, r := range preset.Required {
 			require += r + suffix
 		}
 		require = strings.TrimSuffix(require, suffix)
 		shares := ""
-		for _, s := range own.Subs {
+		for _, s := range preset.Subs {
 			shares += s + suffix
 		}
 		shares = strings.TrimSuffix(shares, suffix)
 		subdomains := ""
-		for _, s := range own.SubDomains {
+		for _, s := range preset.SubDomains {
 			subdomains += s + suffix
 		}
 		pCon.AddObject(widget.NewButton("use", func() {
-			posts, err := getPosts(o, own.Query)
+			posts, err := getPosts(o, preset.Query)
 			if err != nil {
 				o.l.Fatalln(err.Error())
 			}
 			o.canChan <- postCan(o, posts, 0, 50)
 		}))
 		subdomains = strings.TrimSuffix(subdomains, suffix)
-		pCon.AddObject(widget.NewLabel(strconv.FormatBool(own.Candidate)))
-		pCon.AddObject(widget.NewLabel(strconv.Itoa(own.CapPercent)))
-		pCon.AddObject(widget.NewLabel(own.Color))
+		pCon.AddObject(widget.NewLabel(strconv.FormatBool(preset.Candidate)))
+		pCon.AddObject(widget.NewLabel(strconv.Itoa(preset.CapPercent)))
+		pCon.AddObject(widget.NewLabel(preset.Color))
 		pCon.AddObject(widget.NewLabel(discards))
-		pCon.AddObject(widget.NewLabel(strconv.FormatBool(own.Link)))
-		pCon.AddObject(widget.NewLabel(own.Make))
-		pCon.AddObject(widget.NewLabel(strconv.Itoa(own.Odometer)))
-		pCon.AddObject(widget.NewLabel(strconv.Itoa(own.Price)))
+		pCon.AddObject(widget.NewLabel(strconv.FormatBool(preset.Link)))
+		pCon.AddObject(widget.NewLabel(preset.Make))
+		pCon.AddObject(widget.NewLabel(strconv.Itoa(preset.Odometer)))
+		pCon.AddObject(widget.NewLabel(strconv.Itoa(preset.Price)))
 		pCon.AddObject(widget.NewLabel(require))
 		pCon.AddObject(widget.NewLabel(shares))
 		pCon.AddObject(widget.NewLabel(subdomains))
