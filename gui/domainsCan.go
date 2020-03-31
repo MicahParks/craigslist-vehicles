14d4b0e31d32fdcfe5cb775db88de675026403c3
Micah Parks <micahleviparks@gmail.com>
2020-03-31 09:51:10 -0400

    just checkbox.

--- gui/domainsCan.go
+++ gui/domainsCan.go
@@ -14,26 +14,14 @@ var (
 )
 
 func domainsCan(o *orb) *fyne.Container {
-	con := fyne.NewContainerWithLayout(layout.NewGridLayout(3))
+	con := fyne.NewContainerWithLayout(layout.NewGridLayout(2))
 	for k, v := range domains {
 		con.AddObject(widget.NewLabel(k))
-		check := widget.NewCheck("", func(_ bool) {})
-		for _, d := range o.user.Domains {
-			if d == k {
-				check.Checked = true
-				break
-			}
-		}
-		check.Disable()
-		label := "switch"
-		if v {
-			label = "required"
-		}
-		con.AddObject(widget.NewButton(label, func() {
+		check := widget.NewCheck("", func(b bool) {
 			if v {
 				return
 			}
-			if check.Checked {
+			if !b {
 				for i := 0; i < len(o.user.Domains); i++ {
 					if o.user.Domains[i] == k {
 						o.user.Domains = append(o.user.Domains[:i], o.user.Domains[i+1:]...)
@@ -43,12 +31,20 @@ func domainsCan(o *orb) *fyne.Container {
 			} else {
 				o.user.Domains = append(o.user.Domains, k)
 			}
-			check.Checked = !check.Checked
-			check.Refresh()
 			if err := updateDomains(o); err != nil {
 				o.l.Fatalln(err.Error())
 			}
-		}))
+		})
+		for _, d := range o.user.Domains {
+			if d == k {
+				check.Checked = true
+				check.Refresh()
+				break
+			}
+		}
+		if v {
+			check.Disable()
+		}
 		con.AddObject(check)
 	}
 	back := widget.NewButton("back", func() {
